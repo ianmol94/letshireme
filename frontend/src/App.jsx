25773f2da1af526af8b5782e21bb9a461d8bff96
ianmol94 <anmolx46@gmail.com>
2026-08-31 01:32:58 +0530

    small changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -269,6 +269,7 @@ export default function App() {
                 title={label}
               >
                 <Icon />
+                <span>{label}</span>
               </a>
             ))}
           </div>
